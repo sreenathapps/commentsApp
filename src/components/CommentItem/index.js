1b9158db35622876c2bb20f9ed7f1efbfb0f67e9
sai <saisreenath.mandaloju@gmail.com>
2024-12-24 11:47:50 +0000

    fixed some bugs

--- src/components/CommentItem/index.js
+++ src/components/CommentItem/index.js
@@ -1,1 +1,51 @@
-// Write your code here
+import './index.css'
+
+const CommentItem = props => {
+  const {item, onClickDelete, onClickLike} = props
+  const {name, comment, id, bgColor, like} = item
+  const firstLetter = name.charAt(0)
+
+  const handleDelete = () => {
+    onClickDelete(id)
+  }
+
+  const handleLikeButton = () => {
+    onClickLike(id)
+  }
+
+  const likeUrl = `https://assets.ccbp.in/frontend/react-js/comments-app/like${
+    like ? 'd' : ''
+  }-img.png`
+
+  return (
+    <li className="list-item">
+      <div>
+        <div className="li-container">
+          <div className={`circle ${bgColor}`}>{firstLetter}</div>
+          <div className="cont-1">
+            <p className="head-1">{name}</p>
+            <p className="comment-1">{comment}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="last-line">
+        <div className="like">
+          <img className="like-icon" alt="like" src={likeUrl} />
+          <button type="button" onClick={handleLikeButton} className="like-btn">
+            Like
+          </button>
+        </div>
+        <button type="button" onClick={handleDelete} className="delete-btn">
+          <img
+            className="delete-icon"
+            alt="delete"
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png "
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default CommentItem
--- src/components/Comments/index.js
+++ src/components/Comments/index.js
@@ -1,4 +1,6 @@
 import {Component} from 'react'
+import {v4 as uuidv4} from 'uuid'
+import CommentItem from '../CommentItem'
 import './index.css'
 
 const initialContainerBackgroundClassNames = [
@@ -12,11 +14,100 @@ const initialContainerBackgroundClassNames = [
 ]
 
 // Write your code here
-class Comments {
+class Comments extends Component {
   state = {
     comments: [],
     name: '',
     comment: '',
+  }
+
+  onChangeComment = event => {
+    this.setState({comment: event.target.value})
+  }
+
+  onChangeName = event => {
+    this.setState({name: event.target.value})
+  }
+
+  onSubmitForm = event => {
+    event.preventDefault()
+
+    this.setState(prevState => {
+      const {comments, name, comment} = prevState
+      const i = Math.floor(Math.random() * 8)
+      const newComment = {
+        id: uuidv4(),
+        name,
+        comment,
+        bgColor: initialContainerBackgroundClassNames[i],
+        like: false,
+      }
+      return {comments: [...comments, newComment]}
+    })
+  }
+
+  onClickLike = id => {
+    console.log('In comments')
+    this.setState(prevState => ({
+      comments: prevState.comments.map(each => {
+        console.log(`${each.id}  ${id}`)
+        if (id === each.id) {
+          return {...each, like: !each.like}
+        }
+        return each
+      }),
+    }))
+  }
+
+  onClickDelete = id => {
+    this.setState(prevState => ({
+      comments: prevState.comments.filter(each => each.id !== id),
+    }))
+  }
+
+  render() {
+    const {name, comment, comments} = this.state
+
+    return (
+      <div className="bg-container">
+        <div className="container">
+          <form className="form-container" onSubmit={this.onSubmitForm}>
+            <h1 className="heading">Comments</h1>
+            <p className="description">Say something about 4.0 Technologies</p>
+            <input
+              type="text"
+              value={name}
+              onChange={this.onChangeName}
+              className="name-input"
+            />
+            <textarea
+              type="textarea"
+              value={comment}
+              onChange={this.onChangeComment}
+              className="name-input"
+            />
+            <button type="submit" className="submit-btn">
+              Add Comment
+            </button>
+          </form>
+          <img
+            className="image"
+            alt="comments"
+            src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"
+          />
+        </div>
+        <ul className="comments-list">
+          {comments.map(each => (
+            <CommentItem
+              item={each}
+              onClickLike={this.onClickLike}
+              onClickDelete={this.onClickDelete}
+              key={each.id}
+            />
+          ))}
+        </ul>
+      </div>
+    )
   }
 }
 export default Comments
