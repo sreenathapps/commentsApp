22430399aae32e2ccb3bd0d8b702127b8093aded
sai <saisreenath.mandaloju@gmail.com>
2024-12-24 09:10:13 +0000

    first commit

---
+++ src/components/Comments/index.js
@@ -0,0 +1,22 @@
+import {Component} from 'react'
+import './index.css'
+
+const initialContainerBackgroundClassNames = [
+  'amber',
+  'blue',
+  'orange',
+  'emerald',
+  'teal',
+  'red',
+  'light-blue',
+]
+
+// Write your code here
+class Comments {
+  state = {
+    comments: [],
+    name: '',
+    comment: '',
+  }
+}
+export default Comments
